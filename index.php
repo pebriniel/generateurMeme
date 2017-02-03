cdacf12145ed80f8acb5321b193306ca8e4f984c
pebriniel <zeldalyoko@gmail.com>
2017-02-03 10:41:01 +0100

    delete folder __backup

--- index.php
+++ index.php
@@ -3,29 +3,29 @@
 	/*
 		[SERVER_PROTOCOL] => HTTP/1.1
 		[REQUEST_METHOD] => GET
-		[QUERY_STRING] => 
+		[QUERY_STRING] =>
 		[REQUEST_URI] => /class/
 		[SCRIPT_NAME] => /class/index.php
 		[PHP_SELF] => /class/index.php
 		[REQUEST_TIME] => 1482222172
 	*/
-	
+
 	define('PARAM_ID', 3);
-	define('URL_SITE', 'http://boussads.student.codeur.online/memeold/');
+	define('URL_SITE', 'http://boussads.student.codeur.online/meme/');
 
 	require_once('get.class.php');
 	require_once('pdo.class.php');
-	
+
 	include "include/Mustache/Autoloader.php";
-	
+
 	require_once('include/imgManager.class.php');
 	require_once('include/imgMeme.class.php');
-  
-	$db = new SQLpdo("boussads", "boussads", "JqsWM87PwX", "127.0.0.1");
+
+	$db = new SQLpdo("table", "root", "", "127.0.0.1");
 	$app = new getUrl();
 	$app->arrayHeader('URL', URL_SITE);
-	
-	//le generateur 
+
+	//le generateur
 	if($app->get('generator')){
 		if(isset($_REQUEST['generate'])){
 			$sizeTop = $_REQUEST['sizeTop'];
@@ -35,7 +35,7 @@
 			$textTop = $_REQUEST['textTop'];
 			$textBot = $_REQUEST['textBot'];
 			$idImg = $_REQUEST['idImg'];
-			
+
 			$request = $_REQUEST['idImg'];
 			if($request[0] == "h"){
 				$url = $request;
@@ -44,20 +44,20 @@
 				$v = $db->fetch("SELECT * FROM `memeimage` WHERE ID= :id", array(':id' => $request));
 				$url = 'public/img/'.$v['ID'].'.'.$v['type'];
 			}
-			
+
 			$meme = new generateMeme($url);
 			$id_file = uniqid();
-			
+
 			$id_bdd = $db->insert("INSERT INTO memegenerate (url, ID_type) VALUES(:url, :type)", array(':url' => $id_file, ':type' => $meme->getDetail('ext')));
-			
+
 			$meme->setMemeNameFile($id_file);
 			$meme->textTop($_REQUEST['textTop'], $_REQUEST['clrTop']);
 			$meme->textBot($_REQUEST['textBot'], $_REQUEST['clrBot']);
 			$meme->generatMeme('public/uploadImg/', true);
-			
+
 			header('location:'.URL_SITE.'vue/'.$id_bdd);
 		}
-		
+
 		if($id = $app->getParam(PARAM_ID)){
 			$v = $db->fetch("SELECT * FROM `memeimage` WHERE ID= :id", array(':id' => $id));
 			$view = array('SHOW' => true, 'URL_SITE' => URL_SITE, 'URL' => URL_SITE.'public/img/','ID' => $v['ID'], 'FORMAT' => $v['type']);
@@ -65,13 +65,13 @@
 		else{
 			$view = array('SHOW' => false);
 		}
-		
+
 		$app->loadTemplate('creation', $view);
 	}
-	
+
 	//la vue
 	else if($app->get('liste')){
-		
+
 		$v = $db->fetchAll("SELECT memegenerate.ID AS ID, memegenerate.url AS IDGenerate, CASE WHEN memegenerate.ID_memeImage IS NULL THEN memegenerate.ID_type ELSE memeimage.type END AS FORMATIMG FROM `memegenerate` LEFT JOIN memeimage ON memeimage.ID = memegenerate.ID_memeImage ORDER BY memegenerate.ID DESC");
 		$app->loadTemplate('liste', array('SHOW' => true, 'URL' => URL_SITE, 'LIST' => $v));
 	}
@@ -92,7 +92,7 @@
 		$app->loadTemplate('vue', $view);
 	}
 	else if($app->get('ajaxRefreshImage')){
-		
+
 		$image_name = uniqid();
 		if(isset($_GET['idImg'])){
 			$request = $_REQUEST['idImg'];
@@ -103,19 +103,19 @@
 				$v = $db->fetch("SELECT * FROM `memeimage` WHERE ID= :id", array(':id' => $request));
 				$url = 'public/img/'.$v['ID'].'.'.$v['type'];
 			}
-			
+
 			$meme = new generateMeme($url);
 			$meme->setMemeNameFile(md5($_SERVER['REMOTE_ADDR']));
 			$meme->textTop($_REQUEST['textTop'], $_REQUEST['clrTop'], $_REQUEST['sizeTop']);
 			$meme->textBot($_REQUEST['textBot'], $_REQUEST['clrBot'], $_REQUEST['sizeBot']);
 			$meme->generatMeme('public/tmp/', true);
-			
+
 			echo base64_encode(file_get_contents("public/tmp/".md5($_SERVER['REMOTE_ADDR']).".".$meme->getDetail('ext')));
 		}
 	}
-	//l'index 
+	//l'index
 	else if($app->get('/')){
-		
+
 		$val = $val = $db->fetchAll("SELECT * FROM `memeimage`");
 		$app->loadTemplate('main', array('list' => $val));
 	}
